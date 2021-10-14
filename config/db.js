2745114344f847755ac18b02ef8e14ab39d62f83
Ayush Patel <ayush@Ayushs-MacBook-Air.local>
2021-10-14 08:34:54 +0530

    final34e3

--- config/db.js
+++ config/db.js
@@ -5,13 +5,16 @@ const mongoose = require ('mongoose');
 function connectDB(){
     //Database connection
 
-    mongoose.connect(process.env.MONGO_CONNECTION_URL,{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true,useFindAndModify:true});
+    mongoose.connect(process.env.MONGO_CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true});
     const connection = mongoose.connection;
 
     
     connection.once('open',()=>{
         console.log('Database connected.')
-    })
+    }).on('error', (error) => {
+        console.warn('Some error', error);
+      });
+  
 }
 
 module.exports = connectDB;
